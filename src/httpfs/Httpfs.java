73973b40557fbc5cbc45cc515315461dcc12aa8e
ALAIN\Richard <rich.grandmaison@gmail.com>
2016-10-19 19:24:12 -0400

    started work on clientSocket

--- src/httpfs/Httpfs.java
+++ src/httpfs/Httpfs.java
@@ -1,6 +1,11 @@
 package httpfs;
-import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Httpfs {
 	
@@ -18,6 +23,28 @@ public class Httpfs {
 		System.out.println("HTTPFS now listening on port " + port + " ...");
 		if(debugMessages){
 			System.out.println("Debug messages will be printed ...");
+		}
+		
+		//wait for connections
+		while(true){
+			Socket clientSocket = serverSocket.accept();
+			
+			BufferedReader request = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream())); 
+			PrintWriter response = new PrintWriter(new BufferedWriter(
+					new OutputStreamWriter(clientSocket.getOutputStream())),true);
+			
+			String requestLine;
+			while((requestLine = request.readLine()) != null){
+				System.out.println(requestLine);
+			}
+			
+			response.write("k lol");
+			
+			//close all
+			request.close();
+			response.close();
+			clientSocket.close();
 		}
 	}
 }
