84e267e2f761935fbc40d50da3cc50b56b7cf86b
ALAIN\Richard <rich.grandmaison@gmail.com>
2016-10-19 23:04:44 -0400

    Started work on RequestParser

--- src/httpfs/Httpfs.java
+++ src/httpfs/Httpfs.java
@@ -6,6 +6,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Httpfs {
 	
@@ -25,9 +26,24 @@ public class Httpfs {
 			System.out.println("Debug messages will be printed ...");
 		}
 		
+		ArrayList<String> testPost = new ArrayList<String>();
+		testPost.add("POST /path/script.cgi HTTP/1.0\r\n");
+		testPost.add("From: frog@jmarshall.com");
+		testPost.add("User-Agent: HTTPTool/1.0\r\n");
+		testPost.add("Content-Type: application/x-www-form-urlencoded\r\n");
+		testPost.add("Content-Length: 32");
+		testPost.add("\r\n");
+		testPost.add("home=Cosby&favorite+flavor=flies\r\n");
+		
+		RequestParser rp = new RequestParser(testPost);
+		rp.parse();
+		rp.DisplayParsedRequest();
+		
+		/*
 		//wait for connections
 		while(true){
 			Socket clientSocket = serverSocket.accept();
+			System.out.println("New connection accepted ...");
 			
 			BufferedReader request = new BufferedReader(
 					new InputStreamReader(clientSocket.getInputStream())); 
@@ -35,16 +51,20 @@ public class Httpfs {
 					new OutputStreamWriter(clientSocket.getOutputStream())),true);
 			
 			String requestLine;
+			ArrayList<String> fullRequest = new ArrayList<String>();
+			
 			while((requestLine = request.readLine()) != null){
-				System.out.println(requestLine);
+				System.err.println(requestLine);
+				fullRequest.add(requestLine);
 			}
 			
-			response.write("k lol");
+			RequestParser rp = new RequestParser(fullRequest);
+			
 			
 			//close all
 			request.close();
 			response.close();
 			clientSocket.close();
-		}
+		}*/
 	}
 }
--- src/httpfs/RequestParser.java
+++ src/httpfs/RequestParser.java
@@ -1,5 +1,71 @@
 package httpfs;
 
+import java.util.ArrayList;
+
 public class RequestParser {
 
+	ArrayList<String> fullRequest;
+	String method;
+	String path;
+	String host;
+	int port;
+	String contentLength;
+	String contentType;
+	StringBuilder postBody;
+	
+	public RequestParser(ArrayList<String> fr){
+		this.fullRequest = fr;
+		postBody = new StringBuilder();
+	}
+	
+	public void parse(){
+		extractHostAndPort();
+		extractMethodAndPath();
+		if(method.equals("POST")){
+			extractPostHeadersAndBody();
+		}
+	}
+	
+	void extractMethodAndPath() {
+		String[] temp = fullRequest.get(0).split(" ", 3);
+		method = temp[0];
+		path = temp[1];
+	}
+	
+	void extractHostAndPort() {
+		String[] fullHost = fullRequest.get(1).split(" ", 2);
+		String[] splitHost = fullHost[1].split(":", 2);
+		host = splitHost[0];
+		if(splitHost.length > 1)
+			port = Integer.parseInt(splitHost[1]);
+		else port = Httpfs.port;
+	}
+	
+	void extractPostHeadersAndBody(){
+		boolean bodyStarts = false;
+		
+		for(String requestLine : fullRequest){
+			if(requestLine.startsWith("Content-Length")){
+				String[] temp = requestLine.split(": ");
+				contentLength = temp[1];
+			} else if(requestLine.startsWith("Content-Type")){
+				String[] temp = requestLine.split(": ");
+				contentType = temp[1];
+			} else if(requestLine.equals("\r\n")){
+				bodyStarts = true;
+			} else if(bodyStarts){
+				postBody.append(requestLine);
+			}
+		}
+	}
+	
+	public void DisplayParsedRequest(){
+		System.out.println("Method: " + method);
+		System.out.println("Path: " + path);
+		System.out.println("Host: " + host);
+		System.out.println("Port " + port);
+		System.out.println("Content-Length: " + contentLength);
+		System.out.println("Content-Type: " + contentType);
+		System.out.println("Post body: " + postBody.toString());
+	}
 }
